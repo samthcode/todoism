50f879d10f96711aa400cd32f162cc01bced7ec6
Sam Thomas <>
2022-02-17 16:39:16 +0000

    The groundworks.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,31 @@
   import "../app.scss";
 </script>
 
+<nav>
+  <h1>TheTodoism</h1>
+  <a href="/">Home</a>
+  <a href="/about/">About</a>
+</nav>
+
 <slot />
+
+<style lang="scss">
+  a {
+    margin-inline: 1em;
+  }
+  h1 {
+    color: $accent-dark;
+    font-size: 2em;
+    display: inline;
+    margin-right: 1em;
+  }
+  nav {
+    background-color: $bg-light;
+    padding: 0.75em;
+  }
+  a:any-link {
+    font-size: 1.5em;
+    color: $accent-light;
+    text-decoration: none;
+  }
+</style>
---
+++ src/routes/about.svelte
@@ -0,0 +1,10 @@
+<svelte:head><title>About | TheTodoism</title></svelte:head>
+
+<h2>About</h2>
+
+<article>
+  <p>
+    TheTodoism is a homemade todo-list application. There are an abundance of
+    these in the wild, so let me tell you what makes TheTodoism unique:
+  </p>
+</article>
