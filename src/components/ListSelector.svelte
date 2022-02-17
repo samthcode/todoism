3a1d361b47c18359d70405b7b6d888fdcfaad013
Sam Thomas <>
2022-02-17 17:46:13 +0000

    this project was a failure and i quit trying to be a fucking web developer

---
+++ src/components/ListSelector.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let todoLists;
+</script>
+
+{#each todoLists as list}
+  <span class="selector-item">{list.name}</span>
+{/each}
+
+<style lang="scss">
+  .selector-item {
+    background-color: $bg-light;
+    color: $text;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,13 @@
+<script>
+  import ListSelector from "../components/ListSelector.svelte";
+
+  let todoLists = [{ name: "hi" }];
+
+  import { v4 as uuidGeneratorV4 } from "uuid";
+</script>
+
 <svelte:head>
   <title>Home | Todoism</title>
 </svelte:head>
 
-<h2>Welcome to SvelteKit</h2>
+<ListSelector {todoLists} />
