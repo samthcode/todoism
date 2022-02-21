d5019a929c190a69582c604586feddcbffe03c0a
Sam Thomas <>
2022-02-21 17:47:06 +0000

    grid layout in [todo].svelte

--- src/routes/todo/[todo].svelte
+++ src/routes/todo/[todo].svelte
@@ -3,10 +3,10 @@
   import { browser } from "$app/env";
   import { lists } from "$lib/stores/lists";
   import { v4 as uuidGeneratorV4 } from "uuid";
-  let theid = $page.params.todo;
+  let theId = $page.params.todo;
   let todo;
   $: for (const l of $lists) {
-    todo = l.todos.find(({ id }) => id === theid);
+    todo = l.todos.find(({ id }) => id === theId);
     if (todo) {
       break;
     }
@@ -21,23 +21,23 @@
   function updateTodo(newTodo) {
     if (todo) {
       let listIndex = $lists.findIndex(
-        ({ todos }) => !!todos.find(({ id }) => id === theid)
+        ({ todos }) => !!todos.find(({ id }) => id === theId)
       );
       if (listIndex === -1) {
-        console.log(`updateTodo(): Couldn't find todo with id: "${theid}"`);
+        console.log(`updateTodo(): Couldn't find todo with id: "${theId}"`);
         return;
       }
-      if (newTodo.title)
-        $lists[listIndex].todos.find(({ id }) => id === theid).title =
-          newTodo.title;
+      let todoIndex = $lists[listIndex].todos.findIndex(
+        ({ id }) => id === theId
+      );
 
-      if (newTodo.desc)
-        $lists[listIndex].todos.find(({ id }) => id === theid).desc =
-          newTodo.desc;
+      if (newTodo.title)
+        $lists[listIndex].todos[todoIndex].title = newTodo.title;
+
+      if (newTodo.desc) $lists[listIndex].todos[todoIndex].desc = newTodo.desc;
 
       if (newTodo.dueDate)
-        $lists[listIndex].todos.find(({ id }) => id === theid).dueDate =
-          newTodo.dueDate;
+        $lists[listIndex].todos[todoIndex].dueDate = newTodo.dueDate;
 
       $lists = $lists;
     }
@@ -98,32 +98,68 @@
     >Change the title and description to "boo" and "boo!" respectively.</button
   > -->
   {#if todo}
-    <h3 class="small">Title:</h3>
-    <h2>
-      {todo.title}{#if isOverdue(dueDateAsDate) && !todo.completed}<span
-          class="text-warn">&nbsp;(Overdue)</span
-        >{/if}
-    </h2>
-
-    <h3 class="small">Description:</h3>
-    <h3 class="desc">{todo.desc}</h3>
-
-    {#if dueDateFormatted}
-    <h3 class="small">Due Date:</h3>
-      <div class="due-date">Due {dueDateFormatted}</div>
-    {/if}
+    <div class="todo-container">
+      <div class="small title-label">Title:</div>
+      <h2 class="title">
+        {todo.title}{#if isOverdue(dueDateAsDate) && !todo.completed}<span
+            class="text-warn">&nbsp;(Overdue)</span
+          >{/if}
+      </h2>
+      <div class="small desc-label">Description:</div>
+      <div class="desc">{todo.desc}</div>
+      {#if dueDateFormatted}
+        <div class="small due-date-label">Due Date:</div>
+        <div class="due-date">Due {dueDateFormatted}</div>
+      {/if}
+    </div>
   {:else}
-    <h2>Error: Could not find todo with id '{theid}'</h2>
+    <h2>Error: Could not find todo with id '{theId}'</h2>
   {/if}
 </main>
 
 <style>
+  .todo-container {
+    margin-top: 0.75rem;
+    display: grid;
+    column-gap: 1rem;
+    row-gap: 2rem;
+    grid-template-rows: min-content min-content min-content;
+    grid-template-columns: min-content auto;
+    grid-template-areas: "title-label title" "desc-label desc" "due-date-label due-date";
+  }
+
   .small {
-    font-size: 0.9em;
-    margin-bottom: 0.5em;
+    font-size: 14px;
+    margin-bottom: 0.2em;
+  }
+
+  .title {
+    grid-area: title;
+  }
+
+  .title-label {
+    grid-area: title-label;
   }
 
   .desc {
-    margin-bottom: 1.5rem;
+    font-size: 22px;
+    font-weight: bold;
+    grid-area: desc;
+  }
+
+  .desc-label {
+    grid-area: desc-label;
+  }
+
+  .due-date {
+    grid-area: due-date;
+  }
+
+  .due-date-label {
+    grid-area: due-date-label;
+  }
+
+  h2 {
+    margin: 0;
   }
 </style>
