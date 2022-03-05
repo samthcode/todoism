969ea576340c5ac51e2e5201dc273fb25d932acf
Sam Thomas <>
2022-03-05 18:27:50 +0000

    better sizing and spacing for filter options

--- src/lib/components/FilterTodos.svelte
+++ src/lib/components/FilterTodos.svelte
@@ -57,36 +57,50 @@
   }
 </script>
 
-<div class="filter-dialogue">
-  <label for="priority">Priority:</label>
-  <select id="priority" bind:value={$filterValues.priority}>
-    <option value="all">All</option>
-    <option value="none">None</option>
-    <option value="low">Low</option>
-    <option value="medium">Medium</option>
-    <option value="high">High</option>
-  </select>
+<div id="filter-dialogue">
+  <span>
+    <label for="priority">Priority:</label>
+    <select id="priority" bind:value={$filterValues.priority}>
+      <option value="all">All</option>
+      <option value="none">None</option>
+      <option value="low">Low</option>
+      <option value="medium">Medium</option>
+      <option value="high">High</option>
+    </select>
+  </span>
 
-  <label for="status">Status:</label>
-  <select id="status" bind:value={$filterValues.status}>
-    <option value="all">All</option>
-    <option value="todo">Todo</option>
-    <option value="completed">Completed</option>
-  </select>
+  <span>
+    <label for="status">Status:</label>
+    <select id="status" bind:value={$filterValues.status}>
+      <option value="all">All</option>
+      <option value="todo">Todo</option>
+      <option value="completed">Completed</option>
+    </select>
+  </span>
 
-  <label for="maybe">Maybe:</label>
-  <select id="maybe" bind:value={$filterValues.maybe}>
-    <option value="show">Show</option>
-    <option value="hide">Hide</option>
-    <option value="only">Only</option>
-  </select>
+  <span>
+    <label for="maybe">Maybe:</label>
+    <select id="maybe" bind:value={$filterValues.maybe}>
+      <option value="show">Show</option>
+      <option value="hide">Hide</option>
+      <option value="only">Only</option>
+    </select>
+  </span>
 </div>
 
 <style lang="scss">
-  .filter-dialogue {
+  #filter-dialogue {
     margin-bottom: 1.5rem;
     @media only screen and (max-width: $mobile-size) {
       margin-bottom: 0.5rem;
+    }
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    column-gap: 0.5rem;
+    row-gap: 0.5rem;
+    & > span {
+      flex-shrink: 0;
     }
   }
 </style>
